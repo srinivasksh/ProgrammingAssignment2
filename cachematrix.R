b037d0f386055fa007a823112f909875c1386e6e
srinivas.ksh <srinivas.ksheerasagara@gmail.com>
2015-10-25 10:23:27 -0700

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This project is about calculating inverse of an input
+## matrix. However, to avoid same calculations everytime,
+## the output is cached into a dataframe (if one does not exist)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Returns a special vector of various details pertaining to input matrix
+## This is cached to be further used by cacheSolve function
+makeCacheMatrix <- function(x = numeric()) {
+    ## Initialize Inv variable
+    Inv <- NULL
+    set <- function(y) {
+        x <<- y
+        Inv <<- NULL
+    }
+    get <- function() x
+    
+    ## calcInv column variable
+    calcInv <- function(solve) Inv <<- solve
+    
+    ## getInv column variable
+    getInv <- function() Inv
+    
+    ## create a list of set, get, calcInv, and getInv varables
+    list(set = set, get = get, 
+         calcInv = calcInv, 
+         getInv = getInv)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of input matrix. The input matrix data
+## is however given as input in the form of special vector (generated 
+## in the makeCachematrix function)
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## Assign getInv column of dataframe : cached data
+    Inv <- x$getInv()
+    
+    ## Check if cached variable is already available
+    if(!is.null(Inv)) {
+        message("getting cached data")
+        return(Inv)
+    }
+    
+    ## get the matrix data
+    data <- x$get()
+    
+    ## calculate Inverse of the input matrix
+    Inv <- solve(data, ...)
+    
+    ## cache the inverse to x dataframe
+    x$calcInv(Inv)
+    
+    ## return the Inverse
+    Inv
 }
